3f64d6d3552971d236c7e50ca7fecda5ea92e571
Wigumen <martin.wigum@gmail.com>
2021-11-19 02:32:03 +0100

    made terrible terminal hangman

---
+++ src/main.rs
@@ -0,0 +1,125 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    let word_strings = "ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra".split(" ");
+    let words: Vec<&str> = word_strings.collect();
+    let mut rng = rand::thread_rng();
+    let mut won = false;
+    let word: &str = words[rng.gen_range(0..words.len())];
+    println!("Welcome to hangman!\nWill only get the first char of your input.");
+    let mut guesses: usize = 0;
+    let mut correct = String::new();
+    while guesses < 6 {
+        println!("{}", draw_game(word, correct.as_str(), guesses));
+        if correct == word{
+            won = true;
+            break;
+        }
+        let input = get_input();
+        if word.contains(input){
+            correct.push(input);
+            continue;
+        }
+        guesses += 1;
+    }
+
+    println!("{}", draw_game(word, correct.as_str(), guesses));
+
+    if won {
+        println!("Congrats you won!!!!!!");
+    } else {
+        println!("Better luck next time! :)");
+    }
+}
+
+fn draw_game(word: &str, correct: &str, guesses: usize) -> String {
+    let mut outstr = String::new();
+    outstr.push_str(get_ascii(guesses).as_str());
+    outstr.push_str(format!("Wrong guesses: {}\n", guesses).as_str());
+    let mut hidden = String::new();
+    for c in word.chars() {
+        if correct.contains(c){
+            hidden.push(c);
+        } else {
+            hidden.push('_');
+        }
+    }
+    outstr.push_str(format!("Your word: {}", hidden).as_str());
+    return outstr;
+}
+
+fn get_ascii(i: usize) -> String {
+    let hangmanpics = vec![r#"
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+  =========
+    "#,
+    r#"
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+  =========
+    "#,
+    r#"
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+  =========
+    "#,
+    r#"
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+  =========
+    "#,
+    r#"
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+  =========
+    "#,
+    r#"
+    +---+
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |
+  =========
+  "#,
+  r#"
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+"#];
+    return hangmanpics[i].to_string();
+}
+
+fn get_input() -> char {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut buffer).expect("Did not enter a correct string");
+
+    return buffer.chars().next().unwrap();
+}
